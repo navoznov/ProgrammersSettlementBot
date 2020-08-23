f0c00763bed5ced407f24b56aaff68dc8926b1f1
Navoznov, Ivan E <Navoznov.IE@parma.ru>
2020-08-23 15:20:19 +0300

    отображение лога с датой/временем

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import paho.mqtt.subscribe as subscribe
 import time
 import datetime
 
+datetimeFormat = "%Y-%m-%d %H:%M:%S"
+
 # telegram bot settings
 bot_url = "https://api.telegram.org/bot1356767647:AAGH3gR8YqxsFmzrbeIWhmlCsRLHlif5c_k/"
 last_update_id = 0
@@ -16,8 +18,6 @@ topic = "navoznov/outside/temperature"
 temperature_update_interval_sec = 10
 temperature_update_interval = datetime.timedelta(seconds=temperature_update_interval_sec)
 temperature_last_update_datetime = datetime.datetime.now() - datetime.timedelta(days=1)
-
-print(temperature_last_update_datetime)
 
 current_temperature = 999
 
@@ -34,28 +34,37 @@ def get_chat_id(update):
 def get_update_id(update):
     return update['update_id']
 
+
+def printLog(message):
+    now = datetime.datetime.now()
+    strNow = now.strftime(datetimeFormat)
+    print(f"{strNow} {message}")
+
+
 while True:
-    now = datetime.datetime.utcnow()
+    now = datetime.datetime.now()
     if now > temperature_last_update_datetime + temperature_update_interval:
         topics = [topic]
-        print("MQTT: Send request")
+        printLog("MQTT: Send request")
         mqtt_message = subscribe.simple(topics, hostname=mqtt_broker_url, msg_count=1)
         # payload format is b'11.9'
         current_temperature = str(mqtt_message.payload)[2:-1]
-        print("MQTT: Response = " + current_temperature)
+        now = datetime.datetime.now()
+        strNow = now.strftime(datetimeFormat)
+        printLog("MQTT: Response = " + current_temperature)
         temperature_last_update_datetime = now
 
-    params = {'timeout': 30, 'offset': last_update_id + 1 }
-    print("Telegram: Send request")
+    params = {'timeout': 30, 'offset': last_update_id + 1}
+    printLog("Telegram: Send request")
     response = requests.get(bot_url + 'getUpdates', params)
-    responseJson = response.json()    
+    responseJson = response.json()
     results = responseJson['result']
-    print("Telegram: " + str(len(results)) + " results")
+    printLog("Telegram: " + str(len(results)) + " results")
     if len(results) > 0:
         last_update_id = max(map(get_update_id, results))
         chatIds = list(set(map(get_chat_id, results)))
 
         for chat_id in chatIds:
             send_mess(chat_id, 'Сейчас в поселке ' + current_temperature + ' градусов')
-    
+
     time.sleep(10/1000)
