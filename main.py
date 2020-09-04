595a610ac7a5caee7550f056b1f0ed59974be9cc
Navoznov, Ivan E <Navoznov.IE@parma.ru>
2020-09-04 16:21:56 +0300

    изменен вывод лога

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def get_update_id(update):
 def printLog(message):
     now = datetime.datetime.now()
     strNow = now.strftime(datetimeFormat)
-    print(f"{strNow} {message}")
+    print("{} {}".format(strNow, message))
 
 
 while True:
