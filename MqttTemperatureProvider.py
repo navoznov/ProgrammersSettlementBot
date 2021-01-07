1bfa63ce620b6bbc42a97cd7154c109c1167870f
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-01-07 14:11:00 +0300

    добавлена возможность авторизации в mqtt брокере

--- MqttTemperatureProvider.py
+++ MqttTemperatureProvider.py
@@ -7,10 +7,15 @@ import random
 import datetime
 import logging
 
+
 class MqttTemperatureProvider:
-    def __init__(self, mqtt_broker_url, mqtt_topic):
+    def __init__(self, mqtt_broker_url, mqtt_topic, mqtt_client_id = None, mqtt_username = None, mqtt_password = None):
         self.__mqtt_broker_url = mqtt_broker_url
         self.__mqtt_topic = mqtt_topic
+        DEFAULT_CLIENT_ID = 'temperature_bot'
+        self.__mqtt_client_id = DEFAULT_CLIENT_ID if mqtt_client_id == None else mqtt_client_id
+        self.__mqtt_username = mqtt_username
+        self.__mqtt_password = mqtt_password
         self.__temperature = -273.0
         TEMPERATURE_UPDATE_INTERVAL_SEC = 10
         self.__temperature_update_interval = datetime.timedelta(seconds=TEMPERATURE_UPDATE_INTERVAL_SEC)
@@ -21,10 +26,11 @@ class MqttTemperatureProvider:
         if now > self.__temperature_last_update_datetime + self.__temperature_update_interval:
             topics = [self.__mqtt_topic]
             try:
-                mqtt_message = subscribe.simple(topics, hostname=self.__mqtt_broker_url, msg_count=1)
+                auth = self.__get_mqtt_auth()
+                mqtt_message = subscribe.simple(
+                    topics, hostname=self.__mqtt_broker_url, msg_count=1, auth=auth, client_id=self.__mqtt_client_id)
                 # payload format is "b'-11.9'"
                 self.__temperature = str(mqtt_message.payload)[2:-1]
-                self.__temperature_last_update_datetime = now
             except Exception as ex:
                 logging.exception('Ошибка получения температуры')
 
@@ -33,3 +39,8 @@ class MqttTemperatureProvider:
     def get_temperature_last_update_datetime(self):
         return self.__temperature_last_update_datetime
 
+    def __get_mqtt_auth(self):
+        if self.__mqtt_username == None:
+            return None
+
+        return {'username': self.__username, 'password': self.__mqtt_password}
